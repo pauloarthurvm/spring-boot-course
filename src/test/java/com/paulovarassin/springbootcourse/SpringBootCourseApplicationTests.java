96e849e0a57a54961490738ad198b84119d619d4
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-01-29 21:19:06 -0300

    Initial commit

---
+++ src/test/java/com/paulovarassin/springbootcourse/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulovarassin.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
