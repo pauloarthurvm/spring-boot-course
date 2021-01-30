94b355b7206567ee8afd2b787a266663e7e1f2e2
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-01-29 21:45:16 -0300

    Creating RoomRepository

--- src/main/java/com/paulovarassin/springbootcourse/SpringBootCourseApplication.java
+++ src/main/java/com/paulovarassin/springbootcourse/SpringBootCourseApplication.java
@@ -1,13 +1,32 @@
 package com.paulovarassin.springbootcourse;
 
+import com.paulovarassin.springbootcourse.data.entity.Room;
+import com.paulovarassin.springbootcourse.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootCourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCourseApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/rooms")
+    public class RoomController{
+        @Autowired
+        private RoomRepository roomRepository;
+
+        @GetMapping
+        public Iterable<Room> getRooms() {
+            return this.roomRepository.findAll();
+        }
     }
 
 }
